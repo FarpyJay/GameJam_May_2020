41270c918f0c8804155b3f8f837db3b5698ac438
Patrick Farr <pjf1212@gmail.com>
2020-05-08 17:16:15 -0400

    Added basic movement script

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string myName;
+    public float speed = 1.5f;
+    public float panSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive and my name is " + myName);
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
